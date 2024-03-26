019330399ea3f9c67bef576d05d13ca8900271cc
ax-6 <ax-6@outlook.com>
2024-03-26 21:46:42 +0800

    Linked list wait development.

--- compiler/linked_list/linked_list.h
+++ compiler/linked_list/linked_list.h
@@ -6,7 +6,18 @@
 #define AQ_COMPILER_LINKED_LIST_LINKED_LIST_H_
 
 #include "compiler/compiler.h"
+#include "compiler/pair/pair.h"
 
-namespace Aq{}
+namespace Aq{
+template <typename ValueType>
+class Compiler::LinkedList{
+public:
+struct Node{
+Pair::Pair<Node*,Node*> data;
+};
+
+// Wait development.
+};
+}
 
 #endif
