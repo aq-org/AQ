e8ee48ff8be64985d153546bc99f56c8e13078dd
ax-6 <ax-6@outlook.com>
2024-04-11 21:50:26 +0800

    Added symbol table files. Not complete.

---
+++ compiler/symbol_table/symbol_table.cc
@@ -0,0 +1,8 @@
+// Copyright 2024 AQ authors, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#include "compiler/symbol_table/symbol_table.h"
+
+// Wait development.
+namespace Aq {}
---
+++ compiler/symbol_table/symbol_table.h
@@ -0,0 +1,13 @@
+// Copyright 2024 AQ authors, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#ifndef AQ_COMPILER_SYMBOL_TABLE_SYMBOL_TABLE_H_
+#define AQ_COMPILER_SYMBOL_TABLE_SYMBOL_TABLE_H_
+
+#include "compiler/compiler.h"
+
+// Wait development.
+namespace Aq {}
+
+#endif
