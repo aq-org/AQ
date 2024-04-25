e7b51a86e99a2812f79287ec76decf4fc4e24c97
ax-6 <ax-6@outlook.com>
2024-04-25 22:12:20 +0800

    Updated file manipulation functions in AqvmOsIoFile, unfinished.

---
+++ aqvm/os/io/io.h
@@ -0,0 +1,20 @@
+// Copyright 2024 AQ authors, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#ifndef AQ_AQVM_OS_IO_IO_H_
+#define AQ_AQVM_OS_IO_IO_H_
+
+#ifdef __unix__
+#include "aqvm/os/io/file/unix/file.h"
+#endif
+
+// Read a file and return the contents as a char buffer. Returns 0 for success,
+// -1 for operating system file handler not found. other values for errors.
+int AqvmOsIoFile_ReadFile(char *filename, char **buffer);
+
+// Write a file with the contents of a char buffer. Returns 0 for success,
+// -1 for operating system file handler not found. other values for errors.
+int AqvmOsIoFile_WriteFile(char *filename, char *buffer);
+
+#endif
