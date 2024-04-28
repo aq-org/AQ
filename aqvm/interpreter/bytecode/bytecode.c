474aa4f34647e4e391a7652312c154e79d39ab95
ax-6 <ax-6@outlook.com>
2024-04-28 22:20:15 +0800

    Added bytecode running functions. Not complete.

--- aqvm/interpreter/bytecode/bytecode.c
+++ aqvm/interpreter/bytecode/bytecode.c
@@ -3,3 +3,35 @@
 // the root directory.
 
 #include "aqvm/interpreter/bytecode/bytecode.h"
+
+// TODO(Bytecode): Finish these functions after completing bytecode development.
+/*
+int AqvmInterpreterBytecode_NOP(int *registers){}
+int AqvmInterpreterBytecode_LDC(int *registers, int const value){}
+int AqvmInterpreterBytecode_LOAD(int *registers, int const index){}
+int AqvmInterpreterBytecode_STORE(int *registers, int const index, int const value){}
+int AqvmInterpreterBytecode_NEW(int *registers, int const num_elements){}
+int AqvmInterpreterBytecode_FREE(int *registers, int const index){}
+int AqvmInterpreterBytecode_SIZE(int *registers, int const index){}
+int AqvmInterpreterBytecode_ADD(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_SUB(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_MUL(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_DIV(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_MOD(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_NEG(int *registers, int const value){}
+int AqvmInterpreterBytecode_SHL(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_SHR(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_SAR(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_IF(int *registers, int const condition, int const offset){}
+int AqvmInterpreterBytecode_WHILE(int *registers, int const condition, int const offset){}
+int AqvmInterpreterBytecode_NOT(int *registers, int const value){}
+int AqvmInterpreterBytecode_AND(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_OR(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_XOR(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_CMP(int *registers, int const left, int const right){}
+int AqvmInterpreterBytecode_INVOKE(int *registers, int (*function)(int *), int const num_args){}
+int AqvmInterpreterBytecode_RETURN(int *registers, int const value){}
+int AqvmInterpreterBytecode_GOTO(int *registers, int const offset){}
+int AqvmInterpreterBytecode_THROW(int *registers, int const value){}
+int AqvmInterpreterBytecode_WIDE(int *registers){}
+*/
--- aqvm/interpreter/bytecode/bytecode.h
+++ aqvm/interpreter/bytecode/bytecode.h
@@ -36,4 +36,35 @@ enum AqvmInterpreterBytecode_Type {
   WIDE = 0xFF
 };
 
+// TODO(Bytecode): Change these functions after completing bytecode development.
+int AqvmInterpreterBytecode_NOP();
+int AqvmInterpreterBytecode_LDC();
+int AqvmInterpreterBytecode_LOAD();
+int AqvmInterpreterBytecode_STORE();
+int AqvmInterpreterBytecode_NEW();
+int AqvmInterpreterBytecode_FREE();
+int AqvmInterpreterBytecode_SIZE();
+int AqvmInterpreterBytecode_ADD();
+int AqvmInterpreterBytecode_SUB();
+int AqvmInterpreterBytecode_MUL();
+int AqvmInterpreterBytecode_DIV();
+int AqvmInterpreterBytecode_MOD();
+int AqvmInterpreterBytecode_NEG();
+int AqvmInterpreterBytecode_SHL();
+int AqvmInterpreterBytecode_SHR();
+int AqvmInterpreterBytecode_SAR();
+int AqvmInterpreterBytecode_IF();
+int AqvmInterpreterBytecode_WHILE();
+int AqvmInterpreterBytecode_NOT();
+int AqvmInterpreterBytecode_AND();
+int AqvmInterpreterBytecode_OR();
+int AqvmInterpreterBytecode_XOR();
+int AqvmInterpreterBytecode_CMP();
+int AqvmInterpreterBytecode_INVOKE();
+int AqvmInterpreterBytecode_RETURN();
+int AqvmInterpreterBytecode_GOTO();
+int AqvmInterpreterBytecode_THROW();
+int AqvmInterpreterBytecode_WIDE();
+
+
 #endif
