7e9f18b9b754779e546494d71cd6f06acaff3b7d
ax-6 <ax-6@outlook.com>
2024-05-05 22:49:34 +0800

    Changed some files in bytecode.

---
+++ aqvm/interpreter/bytecode/opcode/opcode.c
@@ -0,0 +1,194 @@
+// Copyright 2024 AQ authors, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#include "aqvm/interpreter/bytecode/opcode/opcode.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "aqvm/memory/memory.h"
+#include "aqvm/memory/register.h"
+
+int AqvmInterpreterBytecodeOpcode_NOP() { return 0; }
+
+int AqvmInterpreterBytecodeOpcode_LDC(
+    void* value, struct AqvmMemoryRegister_Register* operand) {
+  switch (operand->type) {
+    case AqvmMemoryRegisterValueType_INT:
+      operand->value.int_value = *(int*)value;
+      break;
+    case AqvmMemoryRegisterValueType_FLOAT:
+      operand->value.float_value = *(float*)value;
+      break;
+    case AqvmMemoryRegisterValueType_DOUBLE:
+      operand->value.double_value = *(double*)value;
+      break;
+    case AqvmMemoryRegisterValueType_LONG:
+      operand->value.long_value = *(long*)value;
+      break;
+    case AqvmMemoryRegisterValueType_CHARACTER:
+      operand->value.character_value = *(char*)value;
+      break;
+    case AqvmMemoryRegisterValueType_BOOLEAN:
+      operand->value.boolean_value = *(bool*)value;
+      break;
+
+    default:
+      // TODO(ERROR): Handle the error of the register value type.
+      return -1;
+  }
+
+  return 0;
+}
+
+int AqvmInterpreterBytecodeOpcode_LOAD(
+    struct AqvmMemoryRegister_Register* ptr,
+    struct AqvmMemoryRegister_Register* operand) {
+  *operand = *ptr;
+  return 0;
+}
+
+int AqvmInterpreterBytecodeOpcode_STORE(
+    struct AqvmMemoryRegister_Register** ptr,
+    struct AqvmMemoryRegister_Register* operand) {
+  *ptr = operand;
+  return 0;
+}
+
+int AqvmInterpreterBytecodeOpcode_NEW(void** ptr, size_t size) {
+  *ptr = AqvmMemory_MemoryAllocation(size);
+  if (*ptr == NULL) {
+    // TODO(WARNING): Handle the warning of memory allocation.
+    return -1;
+  }
+  return 0;
+}
+
+int AqvmInterpreterBytecodeOpcode_FREE(void* ptr, size_t size) {
+  AqvmMemory_FreeMemory(ptr);
+  if (ptr == NULL) {
+    // TODO(WARNING): Handle the warning of memory deallocation.
+    return -1;
+  }
+  return 0;
+}
+
+// TODO(SIZE): Wait to develop this function until the form of this operator has
+// been decided.
+int AqvmInterpreterBytecodeOpcode_SIZE() {}
+
+int AqvmInterpreterBytecodeOpcode_ADD(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {
+  if (operand1->type != operand2->type) {
+    // TODO(ERROR): Handle the error of the register value type.
+    return -1;
+  }
+  result->type = operand1->type;
+  switch (result->type) {
+    case AqvmMemoryRegisterValueType_INT:
+      result->value.int_value =
+          operand1->value.int_value + operand2->value.int_value;
+      break;
+    case AqvmMemoryRegisterValueType_FLOAT:
+      result->value.float_value =
+          operand1->value.float_value + operand2->value.float_value;
+      break;
+    case AqvmMemoryRegisterValueType_DOUBLE:
+      result->value.double_value =
+          operand1->value.double_value + operand2->value.double_value;
+      break;
+    case AqvmMemoryRegisterValueType_LONG:
+      result->value.long_value =
+          operand1->value.long_value + operand2->value.long_value;
+      break;
+
+    default:
+      // TODO(ERROR): Handle the error of the register value type.
+      return -1;
+  }
+  return 0;
+}
+int AqvmInterpreterBytecodeOpcode_SUB(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {
+  if (operand1->type != operand2->type) {
+    // TODO(ERROR): Handle the error of the register value type.
+    return -1;
+  }
+  result->type = operand1->type;
+  switch (result->type) {
+    case AqvmMemoryRegisterValueType_INT:
+      result->value.int_value =
+          operand1->value.int_value - operand2->value.int_value;
+      break;
+    case AqvmMemoryRegisterValueType_FLOAT:
+      result->value.float_value =
+          operand1->value.float_value - operand2->value.float_value;
+      break;
+    case AqvmMemoryRegisterValueType_DOUBLE:
+      result->value.double_value =
+          operand1->value.double_value - operand2->value.double_value;
+      break;
+    case AqvmMemoryRegisterValueType_LONG:
+      result->value.long_value =
+          operand1->value.long_value - operand2->value.long_value;
+      break;
+
+    default:
+      // TODO(ERROR): Handle the error of the register value type.
+      return -1;
+  }
+  return 0;
+}
+
+// TODO(Bytecode): Waiting for development.
+int AqvmInterpreterBytecodeOpcode_MUL(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_DIV(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_MOD(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_NEG(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_SHL(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_SHR(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_SAR(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_IF() {}
+int AqvmInterpreterBytecodeOpcode_WHILE() {}
+int AqvmInterpreterBytecodeOpcode_NOT(bool result, bool operand) {}
+int AqvmInterpreterBytecodeOpcode_AND(bool result, bool operand1, bool operand2) {}
+int AqvmInterpreterBytecodeOpcode_OR(bool result, bool operand1, bool operand2) {}
+int AqvmInterpreterBytecodeOpcode_XOR(bool result, bool operand1, bool operand2) {}
+int AqvmInterpreterBytecodeOpcode_CMP(
+    bool result, int opcode,
+    struct AqvmMemoryRegister_Register * operand1,
+    struct AqvmMemoryRegister_Register* operand2) {}
+int AqvmInterpreterBytecodeOpcode_INVOKE() {}
+int AqvmInterpreterBytecodeOpcode_RETURN() {}
+int AqvmInterpreterBytecodeOpcode_GOTO() {}
+int AqvmInterpreterBytecodeOpcode_THROW() {}
+
+// TODO(WIDE): Update this function if an extended instruction set is
+// required.
+int AqvmInterpreterBytecodeOpcode_WIDE() {}
---
+++ aqvm/interpreter/bytecode/opcode/opcode.h
@@ -0,0 +1,112 @@
+// Copyright 2024 AQ authors, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#ifndef AQ_AQVM_INTERPRETER_BYTECODE_OPCODE_OPCODE_H_
+#define AQ_AQVM_INTERPRETER_BYTECODE_OPCODE_OPCODE_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "aqvm/memory/register.h"
+
+enum AqvmInterpreterBytecodeOpcode_Type {
+  AqvmInterpreterBytecodeOpcodeType_NOP = 0x00,
+  AqvmInterpreterBytecodeOpcodeType_LDC,
+  AqvmInterpreterBytecodeOpcodeType_LOAD,
+  AqvmInterpreterBytecodeOpcodeType_STORE,
+  AqvmInterpreterBytecodeOpcodeType_NEW,
+  AqvmInterpreterBytecodeOpcodeType_FREE,
+  AqvmInterpreterBytecodeOpcodeType_SIZE,
+  AqvmInterpreterBytecodeOpcodeType_ADD,
+  AqvmInterpreterBytecodeOpcodeType_SUB,
+  AqvmInterpreterBytecodeOpcodeType_MUL,
+  AqvmInterpreterBytecodeOpcodeType_DIV,
+  AqvmInterpreterBytecodeOpcodeType_MOD,
+  AqvmInterpreterBytecodeOpcodeType_NEG,
+  AqvmInterpreterBytecodeOpcodeType_SHL,
+  AqvmInterpreterBytecodeOpcodeType_SHR,
+  AqvmInterpreterBytecodeOpcodeType_SAR,
+  AqvmInterpreterBytecodeOpcodeType_IF,
+  AqvmInterpreterBytecodeOpcodeType_WHILE,
+  AqvmInterpreterBytecodeOpcodeType_NOT,
+  AqvmInterpreterBytecodeOpcodeType_AND,
+  AqvmInterpreterBytecodeOpcodeType_OR,
+  AqvmInterpreterBytecodeOpcodeType_XOR,
+  AqvmInterpreterBytecodeOpcodeType_CMP,
+  AqvmInterpreterBytecodeOpcodeType_INVOKE,
+  AqvmInterpreterBytecodeOpcodeType_RETURN,
+  AqvmInterpreterBytecodeOpcodeType_GOTO,
+  AqvmInterpreterBytecodeOpcodeType_THROW,
+  AqvmInterpreterBytecodeOpcodeType_WIDE = 0xFF
+};
+
+// TODO(Bytecode): Change these functions after completing bytecode development.
+int AqvmInterpreterBytecodeOpcode_NOP();
+int AqvmInterpreterBytecodeOpcode_LDC(
+    void* value, struct AqvmMemoryRegister_Register* operand);
+int AqvmInterpreterBytecodeOpcode_LOAD(
+    struct AqvmMemoryRegister_Register* ptr,
+    struct AqvmMemoryRegister_Register* operand);
+int AqvmInterpreterBytecodeOpcode_STORE(
+    struct AqvmMemoryRegister_Register** ptr,
+    struct AqvmMemoryRegister_Register* operand);
+int AqvmInterpreterBytecodeOpcode_NEW(void** ptr, size_t size);
+int AqvmInterpreterBytecodeOpcode_FREE(void* ptr, size_t size);
+int AqvmInterpreterBytecodeOpcode_SIZE();
+int AqvmInterpreterBytecodeOpcode_ADD(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_SUB(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_MUL(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_DIV(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_MOD(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_NEG(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_SHL(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_SHR(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_SAR(
+    struct AqvmMemoryRegister_Register* result,
+    struct AqvmMemoryRegister_Register* operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_IF();
+int AqvmInterpreterBytecodeOpcode_WHILE();
+int AqvmInterpreterBytecodeOpcode_NOT(bool result, bool operand);
+int AqvmInterpreterBytecodeOpcode_AND(bool result, bool operand1,
+                                      bool operand2);
+int AqvmInterpreterBytecodeOpcode_OR(bool result, bool operand1, bool operand2);
+int AqvmInterpreterBytecodeOpcode_XOR(bool result, bool operand1,
+                                      bool operand2);
+int AqvmInterpreterBytecodeOpcode_CMP(
+    bool result, int opcode, struct AqvmMemoryRegister_Register * operand1,
+    struct AqvmMemoryRegister_Register* operand2);
+int AqvmInterpreterBytecodeOpcode_INVOKE();
+int AqvmInterpreterBytecodeOpcode_RETURN();
+int AqvmInterpreterBytecodeOpcode_GOTO();
+int AqvmInterpreterBytecodeOpcode_THROW();
+
+// TODO(WIDE): Update this function if an extended instruction set is required.
+int AqvmInterpreterBytecodeOpcode_WIDE();
+
+#endif
