b628a2ead227e8287d609eddf6a0134b4bca7461
ax-6 <ax-6@outlook.com>
2024-06-15 16:32:44 +0800

    Fixed some errors.

--- aqvm/interpreter/bytecode/opcode/opcode.h
+++ aqvm/interpreter/bytecode/opcode/opcode.h
@@ -5,9 +5,10 @@
 #ifndef AQ_AQVM_INTERPRETER_BYTECODE_OPCODE_OPCODE_H_
 #define AQ_AQVM_INTERPRETER_BYTECODE_OPCODE_OPCODE_H_
 
-#include <stdbool.h>
 #include <stddef.h>
 
+#include "aqvm/memory/types.h"
+#include "aqvm/memory/memory.h"
 
 enum AqvmInterpreterBytecodeOpcode_Type {
   AqvmInterpreterBytecodeOpcodeType_NOP = 0x00,
@@ -40,50 +41,50 @@ enum AqvmInterpreterBytecodeOpcode_Type {
 // TODO(Bytecode): Change these functions after completing bytecode development.
 int AqvmInterpreterBytecodeOpcode_NOP();
 int AqvmInterpreterBytecodeOpcode_LOAD(
-    struct AqvmMemoryRegister_Register* ptr,
-    struct AqvmMemoryRegister_Register* operand);
+    struct AqvmMemory_Memory* ptr,
+    struct AqvmMemory_Memory* operand);
 int AqvmInterpreterBytecodeOpcode_STORE(
-    struct AqvmMemoryRegister_Register** ptr,
-    struct AqvmMemoryRegister_Register* operand);
+    struct AqvmMemory_Memory** ptr,
+    struct AqvmMemory_Memory* operand);
 int AqvmInterpreterBytecodeOpcode_NEW(void** ptr, size_t size);
 int AqvmInterpreterBytecodeOpcode_FREE(void* ptr, size_t size);
 int AqvmInterpreterBytecodeOpcode_SIZE();
 int AqvmInterpreterBytecodeOpcode_ADD(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_SUB(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_MUL(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_DIV(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_REM(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_NEG(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_SHL(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_SHR(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_SAR(
-    struct AqvmMemoryRegister_Register* result,
-    struct AqvmMemoryRegister_Register* operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    struct AqvmMemory_Memory* result,
+    struct AqvmMemory_Memory* operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_IF();
 int AqvmInterpreterBytecodeOpcode_AND(bool result, bool operand1,
                                       bool operand2);
@@ -91,8 +92,8 @@ int AqvmInterpreterBytecodeOpcode_OR(bool result, bool operand1, bool operand2);
 int AqvmInterpreterBytecodeOpcode_XOR(bool result, bool operand1,
                                       bool operand2);
 int AqvmInterpreterBytecodeOpcode_CMP(
-    bool result, int opcode, struct AqvmMemoryRegister_Register * operand1,
-    struct AqvmMemoryRegister_Register* operand2);
+    bool result, int opcode, struct AqvmMemory_Memory * operand1,
+    struct AqvmMemory_Memory* operand2);
 int AqvmInterpreterBytecodeOpcode_INVOKE();
 int AqvmInterpreterBytecodeOpcode_RETURN();
 int AqvmInterpreterBytecodeOpcode_GOTO();
