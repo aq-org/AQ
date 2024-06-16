68f26b0fdf7d251caa78ffff4c3ff3e2b53f91dd
ax-6 <ax-6@outlook.com>
2024-06-16 20:39:00 +0800

    Added the type.

--- aqvm/memory/types.h
+++ aqvm/memory/types.h
@@ -8,11 +8,19 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// 0x00 is NULL type
+
+// 0x01
 typedef int32_t aqint;
+// 0x02
 typedef int64_t aqlong;
+// 0x03
 typedef float aqfloat;
+// 0x04
 typedef double aqdouble;
+// 0x05
 typedef uint8_t aqchar;
+// 0x06
 typedef bool aqbool;
 
 #endif
