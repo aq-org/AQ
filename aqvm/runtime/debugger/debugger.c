3ac51d34a95981235ce9060657846b40b0d9a60d
ax-6 <ax-6@outlook.com>
2024-06-26 00:37:38 +0800

    Updated the code of Debugger. Not complete.

---
+++ aqvm/runtime/debugger/debugger.c
@@ -0,0 +1,14 @@
+// Copyright 2024 AQ authors, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#include "aqvm/runtime/debugger/debugger.h"
+
+void AqvmRuntimeDebugger_ReportError(const char* error_code,
+                                     const char* error_message) {}
+
+void AqvmRuntimeDebugger_ReportWarning(const char* warning_code,
+                                       const char* warning_message) {}
+
+void AqvmRuntimeDebugger_ReportInfo(const char* info_code,
+                                    const char* info_message) {}
---
+++ aqvm/runtime/debugger/debugger.h
@@ -0,0 +1,23 @@
+// Copyright 2024 AQ authors, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#ifndef AQ_AQVM_RUNTIME_DEBUGGER_DEBUGGER_H_
+#define AQ_AQVM_RUNTIME_DEBUGGER_DEBUGGER_H_
+
+// Reports an error. It needs |error_code| and |error_message| and outputs them
+// with other information. No return.
+void AqvmRuntimeDebugger_ReportError(const char* error_code,
+                                     const char* error_message);
+
+// Reports a warning. It needs |warning_code| and |warning_message| and outputs
+// them with other information. No return.
+void AqvmRuntimeDebugger_ReportWarning(const char* warning_code,
+                                       const char* warning_message);
+
+// Reports an info. It needs |info_code| and |info_message| and outputs them
+// with other information. No return.
+void AqvmRuntimeDebugger_ReportInfo(const char* info_code,
+                                    const char* info_message);
+
+#endif
