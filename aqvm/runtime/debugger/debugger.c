2557a96a7c13d282299ac2e9947b3be8f5ad94e6
ax-6 <ax-6@outlook.com>
2024-06-28 00:20:21 +0800

    Developed the dubugger in the runtime. Not complete.

--- aqvm/runtime/debugger/debugger.c
+++ aqvm/runtime/debugger/debugger.c
@@ -4,11 +4,27 @@
 
 #include "aqvm/runtime/debugger/debugger.h"
 
+#include <errno.h>
+#include <stddef.h>
+#include <string.h>
+
 void AqvmRuntimeDebugger_ReportError(const char* error_code,
-                                     const char* error_message) {}
+                                     const char* error_message, ...) {
+  size_t message_size = 0;
+  message_size = strlen(error_code) + strlen(error_message);
+  va_list args;
+  va_start(args, error_message);
+  char* message = malloc(message_size + 1);
+  vsnprintf(message, message_size + 1, error_message, args);
+  va_end(args);
+  fprintf(stderr(), "%s\n", message);
+  free(message);
+}
 
 void AqvmRuntimeDebugger_ReportWarning(const char* warning_code,
-                                       const char* warning_message) {}
+                                       const char* warning_message, ...) {}
 
 void AqvmRuntimeDebugger_ReportInfo(const char* info_code,
-                                    const char* info_message) {}
+                                    const char* info_message, ...) {}
+
+void AqvmRuntimeDebugger_Output(const char* debug_message) {}
--- aqvm/runtime/debugger/debugger.h
+++ aqvm/runtime/debugger/debugger.h
@@ -5,19 +5,24 @@
 #ifndef AQ_AQVM_RUNTIME_DEBUGGER_DEBUGGER_H_
 #define AQ_AQVM_RUNTIME_DEBUGGER_DEBUGGER_H_
 
+#include <stdarg.h>
+
 // Reports an error. It needs |error_code| and |error_message| and outputs them
 // with other information. No return.
 void AqvmRuntimeDebugger_ReportError(const char* error_code,
-                                     const char* error_message);
+                                     const char* error_message, ...);
 
 // Reports a warning. It needs |warning_code| and |warning_message| and outputs
 // them with other information. No return.
 void AqvmRuntimeDebugger_ReportWarning(const char* warning_code,
-                                       const char* warning_message);
+                                       const char* warning_message, ...);
 
 // Reports an info. It needs |info_code| and |info_message| and outputs them
 // with other information. No return.
 void AqvmRuntimeDebugger_ReportInfo(const char* info_code,
-                                    const char* info_message);
+                                    const char* info_message, ...);
+
+// Outputs |debugg_message|. No return.
+void AqvmRuntimeDebugger_Output(const char* debug_message);
 
 #endif
