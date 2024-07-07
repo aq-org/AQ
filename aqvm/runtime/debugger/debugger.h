c4e401556340460f8e9fdd3272303d31a18129f3
ax-6 <ax-6@outlook.com>
2024-07-07 21:09:53 +0800

    Updated code implementation of reports about the debugger.

--- aqvm/runtime/debugger/debugger.h
+++ aqvm/runtime/debugger/debugger.h
@@ -22,9 +22,15 @@ struct AqvmRuntimeDebugger_DebugReport {
 
 // Outputs |report| with time, errno and so on to printing to the console or
 // other devices and writing to a log file. No return.
-// NOTICE: The output is json format.
-void AqvmRuntimeDebugger_OutputReport(
-    struct AqvmRuntimeDebugger_DebugReport report);
+// |type| is the type of the report. In |type|, 0 is INFO, 1 is WARNING, and 2
+// is ERROR. |type|, |code|, and |message| are necessary and shouldn't be set to
+// NULL in common. |other_info| is a pointer to the other information and it can
+// be set to NULL if it is not needed.
+// NOTICE: If you need to use it, please use json format. The output is json
+// format.
+void AqvmRuntimeDebugger_OutputReport(uint8_t type, const char* code,
+                                      const char* message,
+                                      const char* other_info);
 
 // INTERNAL USE ONLY.
 // Gets the current time in a string format based on ISO 8601 standard and
@@ -34,11 +40,11 @@ void AqvmRuntimeDebugger_OutputReport(
 void AqvmRuntimeDebugger_GetCurrentTime(char* return_time);
 
 // INTERNAL USE ONLY.
-// Formats |report| and returns type string accroding to the type in |report|.
-// If code, message, or other_info are NULL, They will be set to the string
-// "NULL".
+// If |code|, |message|, or |other_info| is NULL, It will be set to the string
+// "NULL". Returns type string accroding to the |type|.
 // NOTICE: The output is json format.
-const char* AqvmRuntimeDebugger_FormatReport(
-    struct AqvmRuntimeDebugger_DebugReport* report);
+const char* AqvmRuntimeDebugger_FormatReport(uint8_t type, const char* code,
+                                             const char* message,
+                                             const char* other_info);
 
 #endif
