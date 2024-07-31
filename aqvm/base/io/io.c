9bb4856f09fb3894793d7c5da7325054ce0c2cf2
ax-6 <ax-6@outlook.com>
2024-07-31 23:44:49 +0800

    The functions related to input and output have been reordered.

--- aqvm/base/io/io.c
+++ aqvm/base/io/io.c
@@ -43,6 +43,10 @@ int AqvmBaseIo_CloseIo() {
   return 0;
 }
 
+int AqvmBaseIo_fgetc(struct AqvmBaseFile_File* stream);
+
+char* AqvmBaseIo_fgets(char* str, int n, struct AqvmBaseFile_File* stream);
+
 int AqvmBaseIo_fprintf(struct AqvmBaseFile_File* stream, const char* format,
                        ...) {
   if (stream == NULL || stream->file == NULL || format == NULL) {
@@ -95,10 +99,19 @@ int AqvmBaseIo_fprintf(struct AqvmBaseFile_File* stream, const char* format,
   return 0;
 }
 
-int AqvmBaseIo_fscanf(struct AqvmBaseFile_File* stream, const char* format,
-                      ...) {}
+int AqvmBaseIo_fputc(int character, struct AqvmBaseFile_File* stream);
 
-int AqvmBaseIo_perror(const char* str) {}
+int AqvmBaseIo_fputs(const char* str, struct AqvmBaseFile_File* stream);
+
+int AqvmBaseIo_fscanf(struct AqvmBaseFile_File* stream, const char* format, ...);
+
+int AqvmBaseIo_getc(struct AqvmBaseFile_File* stream);
+
+int AqvmBaseIo_getchar(void);
+
+char* AqvmBaseIo_gets(char* str);
+
+int AqvmBaseIo_perror(const char* str);
 
 int AqvmBaseIo_printf(const char* format, ...) {
   if (format == NULL) {
@@ -126,13 +139,21 @@ int AqvmBaseIo_printf(const char* format, ...) {
   return 0;
 }
 
-int AqvmBaseIo_scanf(const char* format, ...) {}
+int AqvmBaseIo_putc(int character, struct AqvmBaseFile_File* stream);
 
-int AqvmBaseIo_snprintf(char* str, size_t size, const char* format, ...) {}
+int AqvmBaseIo_putchar(int character);
 
-int AqvmBaseIo_sprintf(char* str, const char* format, ...) {}
+int AqvmBaseIo_puts(const char* str);
 
-int AqvmBaseIo_sscanf(const char* str, const char* format, ...) {}
+int AqvmBaseIo_scanf(const char* format, ...);
+
+int AqvmBaseIo_snprintf(char* str, size_t size, const char* format, ...);
+
+int AqvmBaseIo_sprintf(char* str, const char* format, ...);
+
+int AqvmBaseIo_sscanf(const char* str, const char* format, ...);
+
+int AqvmBaseIo_ungetc(int character, struct AqvmBaseFile_File* stream);
 
 int AqvmBaseIo_vfprintf(struct AqvmBaseFile_File* stream, const char* format,
                         va_list arg) {
@@ -208,26 +229,4 @@ int AqvmBaseIo_vprintf(const char* format, va_list arg) {
   return 0;
 }
 
-int AqvmBaseIo_vsprintf(char* str, const char* format, va_list arg) {}
-
-int AqvmBaseIo_fgetc(struct AqvmBaseFile_File* stream) {}
-
-char* AqvmBaseIo_fgets(char* str, int n, struct AqvmBaseFile_File* stream) {}
-
-int AqvmBaseIo_fputc(int character, struct AqvmBaseFile_File* stream) {}
-
-int AqvmBaseIo_fputs(const char* str, struct AqvmBaseFile_File* stream) {}
-
-int AqvmBaseIo_getc(struct AqvmBaseFile_File* stream) {}
-
-int AqvmBaseIo_getchar(void) {}
-
-char* AqvmBaseIo_gets(char* str) {}
-
-int AqvmBaseIo_putc(int character, struct AqvmBaseFile_File* stream) {}
-
-int AqvmBaseIo_putchar(int character) {}
-
-int AqvmBaseIo_puts(const char* str) {}
-
-int AqvmBaseIo_ungetc(int character, struct AqvmBaseFile_File* stream) {}
+int AqvmBaseIo_vsprintf(char* str, const char* format, va_list arg);
--- aqvm/base/io/io.h
+++ aqvm/base/io/io.h
@@ -15,13 +15,33 @@ int AqvmBaseIo_InitializeIo();
 
 int AqvmBaseIo_CloseIo();
 
+int AqvmBaseIo_fgetc(struct AqvmBaseFile_File* stream);
+
+char* AqvmBaseIo_fgets(char* str, int n, struct AqvmBaseFile_File* stream);
+
 int AqvmBaseIo_fprintf(struct AqvmBaseFile_File* stream, const char* format, ...);
 
+int AqvmBaseIo_fputc(int character, struct AqvmBaseFile_File* stream);
+
+int AqvmBaseIo_fputs(const char* str, struct AqvmBaseFile_File* stream);
+
 int AqvmBaseIo_fscanf(struct AqvmBaseFile_File* stream, const char* format, ...);
+
+int AqvmBaseIo_getc(struct AqvmBaseFile_File* stream);
+
+int AqvmBaseIo_getchar(void);
+
+char* AqvmBaseIo_gets(char* str);
 
 int AqvmBaseIo_perror(const char* str);
 
 int AqvmBaseIo_printf(const char* format, ...);
+
+int AqvmBaseIo_putc(int character, struct AqvmBaseFile_File* stream);
+
+int AqvmBaseIo_putchar(int character);
+
+int AqvmBaseIo_puts(const char* str);
 
 int AqvmBaseIo_scanf(const char* format, ...);
 
@@ -31,32 +51,12 @@ int AqvmBaseIo_sprintf(char* str, const char* format, ...);
 
 int AqvmBaseIo_sscanf(const char* str, const char* format, ...);
 
+int AqvmBaseIo_ungetc(int character, struct AqvmBaseFile_File* stream);
+
 int AqvmBaseIo_vfprintf(struct AqvmBaseFile_File* stream, const char* format, va_list arg);
 
 int AqvmBaseIo_vprintf(const char* format, va_list arg);
 
 int AqvmBaseIo_vsprintf(char* str, const char* format, va_list arg);
-
-int AqvmBaseIo_fgetc(struct AqvmBaseFile_File* stream);
-
-char* AqvmBaseIo_fgets(char* str, int n, struct AqvmBaseFile_File* stream);
-
-int AqvmBaseIo_fputc(int character, struct AqvmBaseFile_File* stream);
-
-int AqvmBaseIo_fputs(const char* str, struct AqvmBaseFile_File* stream);
-
-int AqvmBaseIo_getc(struct AqvmBaseFile_File* stream);
-
-int AqvmBaseIo_getchar(void);
-
-char* AqvmBaseIo_gets(char* str);
-
-int AqvmBaseIo_putc(int character, struct AqvmBaseFile_File* stream);
-
-int AqvmBaseIo_putchar(int character);
-
-int AqvmBaseIo_puts(const char* str);
-
-int AqvmBaseIo_ungetc(int character, struct AqvmBaseFile_File* stream);
 
 #endif
