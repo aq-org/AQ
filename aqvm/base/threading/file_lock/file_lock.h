87fe5b074176a76882eeb33bcb78b06bc42a4ab4
ax-6 <ax-6@outlook.com>
2024-08-02 23:40:07 +0800

    Added file lock to threading.

---
+++ aqvm/base/threading/file_lock/file_lock.h
@@ -0,0 +1,8 @@
+// Copyright 2024 AQ author, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#ifndef AQ_AQVM_BASE_THREADING_FILE_LOCK_FILE_LOCK_H_
+#define AQ_AQVM_BASE_THREADING_FILE_LOCK_FILE_LOCK_H_
+
+#endif
