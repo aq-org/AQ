8f642660e0647cb2d0601dec9661254d422519d1
ax-6 <ax-6@outlook.com>
2024-08-04 14:14:59 +0800

    Added hash table in base.

---
+++ aqvm/base/hash/table/table.h
@@ -0,0 +1,8 @@
+// Copyright 2024 AQ author, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#ifndef AQ_AQVM_BASE_HASH_TABLE_TABLE_H_
+#define AQ_AQVM_BASE_HASH_TABLE_TABLE_H_
+
+#endif
