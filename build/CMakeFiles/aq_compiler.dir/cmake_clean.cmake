734fe4be9f348a0ed17739881406fb04acd533d6
Xie Zicong <ax-6@outlook.com>
2025-05-02 12:59:36 +0000

    Fixed some code about CPython.

---
+++ build/CMakeFiles/aq_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aq_compiler.dir/prototype/compiler.cc.o"
+  "aq_compiler"
+  "aq_compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aq_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/third-party/cpython/CMakeFiles/cpython_build.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpython_build"
+  "CMakeFiles/cpython_build-complete"
+  "cpython_build-prefix/src/cpython_build-stamp/cpython_build-build"
+  "cpython_build-prefix/src/cpython_build-stamp/cpython_build-configure"
+  "cpython_build-prefix/src/cpython_build-stamp/cpython_build-download"
+  "cpython_build-prefix/src/cpython_build-stamp/cpython_build-install"
+  "cpython_build-prefix/src/cpython_build-stamp/cpython_build-mkdir"
+  "cpython_build-prefix/src/cpython_build-stamp/cpython_build-patch"
+  "cpython_build-prefix/src/cpython_build-stamp/cpython_build-update"
+  "install/lib/libpython3.14.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cpython_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
