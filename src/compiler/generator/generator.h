5a0d9d6f3fce9af2e85a0db671456dcfec09d2df
Xie Zicong <ax-6@outlook.com>
2025-06-03 17:45:44 +0800

    Recovered a change about Token from compiler.cc in path prototype. And added a new class Generator, NOT COMPLETE.

---
+++ src/compiler/generator/generator.h
@@ -0,0 +1,20 @@
+// Copyright 2025 AQ author, All Rights Reserved.
+// This program is licensed under the AQ License. You can find the AQ license in
+// the root directory.
+
+#ifndef AQ_COMPILER_GENERATOR_GENERATOR_H_
+#define AQ_COMPILER_GENERATOR_GENERATOR_H_
+
+namespace Aq {
+namespace Compiler {
+class Generaor {
+ public:
+  Generaor() = default;
+  virtual ~Generaor() = default;
+
+  
+};
+}  // namespace Compiler
+}  // namespace Aq
+
+#endif
