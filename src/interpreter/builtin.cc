1305f3d98ef014e40e0f175fb9e2a01162f0d7fb
Xie Zicong <ax-6@outlook.com>
2025-08-31 21:41:23 +0800

    Added many math related functions.

--- src/interpreter/builtin.cc
+++ src/interpreter/builtin.cc
@@ -4,6 +4,7 @@
 
 #include "interpreter/builtin.h"
 
+#include <cmath>
 #include <cstdio>
 #include <string>
 
@@ -38,51 +39,51 @@ void InitBuiltInFunctionDeclaration(Interpreter& interpreter) {
                                 __builtin_void);
 
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_acos",
-                                __builtin_void);
+                                __builtin_math_acos);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_asin",
-                                __builtin_void);
+                                __builtin_math_asin);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_atan",
-                                __builtin_void);
+                                __builtin_math_atan);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_atan2",
-                                __builtin_void);
+                                __builtin_math_atan2);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_ceil",
-                                __builtin_void);
+                                __builtin_math_ceil);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_cos",
-                                __builtin_void);
+                                __builtin_math_cos);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_cosh",
-                                __builtin_void);
+                                __builtin_math_cosh);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_exp",
-                                __builtin_void);
+                                __builtin_math_exp);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_fabs",
-                                __builtin_void);
+                                __builtin_math_fabs);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_floor",
-                                __builtin_void);
+                                __builtin_math_floor);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_fmod",
-                                __builtin_void);
+                                __builtin_math_fmod);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_frexp",
-                                __builtin_void);
+                                __builtin_math_frexp);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_hypot",
-                                __builtin_void);
+                                __builtin_math_hypot);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_ldexp",
-                                __builtin_void);
+                                __builtin_math_ldexp);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_log",
-                                __builtin_void);
+                                __builtin_math_log);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_log10",
-                                __builtin_void);
+                                __builtin_math_log10);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_modf",
-                                __builtin_void);
+                                __builtin_math_modf);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_pow",
-                                __builtin_void);
+                                __builtin_math_pow);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_sin",
-                                __builtin_void);
+                                __builtin_math_sin);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_sinh",
-                                __builtin_void);
+                                __builtin_math_sinh);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_sqrt",
-                                __builtin_void);
+                                __builtin_math_sqrt);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_tan",
-                                __builtin_void);
+                                __builtin_math_tan);
   AddBuiltInFunctionDeclaration(interpreter, "__builtin_math_tanh",
-                                __builtin_void);
+                                __builtin_math_tanh);
 }
 
 int __builtin_void(Memory* memory, std::vector<std::size_t> arguments) {
@@ -251,7 +252,8 @@ int __builtin_read(Memory* memory, std::vector<std::size_t> arguments) {
   fseek(file_ptr, 0, SEEK_SET);
 
   char* temp_ptr = new char[file_size + 1];
-  std::size_t actually_read = fread(temp_ptr, sizeof(char), file_size, file_ptr);
+  std::size_t actually_read =
+      fread(temp_ptr, sizeof(char), file_size, file_ptr);
   temp_ptr[actually_read] = '\0';
 
   std::string result = temp_ptr;
@@ -300,94 +302,225 @@ int __builtin_GUI_CreateWindow(Memory* memory,
 }
 
 int __builtin_math_acos(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::acos(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_asin(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::asin(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_atan(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::atan(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_atan2(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 3) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double y = GetDouble(memory_ptr + arguments[1]);
+  double x = GetDouble(memory_ptr + arguments[2]);
+  SetDouble(memory_ptr + arguments[0], std::atan2(y, x));
   return 0;
 }
 
 int __builtin_math_ceil(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::ceil(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_cos(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::cos(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_cosh(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::cosh(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_exp(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::exp(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_fabs(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::fabs(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_floor(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::floor(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_fmod(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 3) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double numer = GetDouble(memory_ptr + arguments[1]);
+  double denom = GetDouble(memory_ptr + arguments[2]);
+  SetDouble(memory_ptr + arguments[0], std::fmod(numer, denom));
   return 0;
 }
 
 int __builtin_math_frexp(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double value = GetDouble(memory_ptr + arguments[1]);
+  int exp;
+  double fraction = std::frexp(value, &exp);
+
+  Object fraction_object;
+  fraction_object.type = 3;
+  fraction_object.data.float_data = fraction;
+  fraction_object.constant_type = true;
+
+  Object exp_object;
+  exp_object.type = 2;
+  exp_object.data.int_data = exp;
+  exp_object.constant_type = true;
+
+  Memory* array_memory = new Memory();
+  array_memory->GetMemory().push_back(fraction_object);
+  array_memory->GetMemory().push_back(exp_object);
+
+  SetArray(memory_ptr + arguments[0], array_memory);
   return 0;
 }
 
 int __builtin_math_hypot(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 3) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double x = GetDouble(memory_ptr + arguments[1]);
+  double y = GetDouble(memory_ptr + arguments[2]);
+  SetDouble(memory_ptr + arguments[0], std::hypot(x, y));
   return 0;
 }
 
 int __builtin_math_ldexp(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 3) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double x = GetDouble(memory_ptr + arguments[1]);
+  int exp = static_cast<int>(GetLong(memory_ptr + arguments[2]));
+  SetDouble(memory_ptr + arguments[0], std::ldexp(x, exp));
   return 0;
 }
 
 int __builtin_math_log(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::log(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_log10(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::log10(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_modf(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double value = GetDouble(memory_ptr + arguments[1]);
+  double int_part;
+  double frac = std::modf(value, &int_part);
+
+  Object frac_object;
+  frac_object.type = 3;
+  frac_object.data.float_data = frac;
+  frac_object.constant_type = true;
+
+  Object int_part_object;
+  int_part_object.type = 2;
+  int_part_object.data.int_data = int_part;
+  int_part_object.constant_type = true;
+
+  Memory* array_memory = new Memory();
+  array_memory->GetMemory().push_back(frac_object);
+  array_memory->GetMemory().push_back(int_part_object);
+
+  SetArray(memory_ptr + arguments[0], array_memory);
   return 0;
 }
 
 int __builtin_math_pow(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 3) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double base = GetDouble(memory_ptr + arguments[1]);
+  double exponent = GetDouble(memory_ptr + arguments[2]);
+  SetDouble(memory_ptr + arguments[0], std::pow(base, exponent));
   return 0;
 }
 
 int __builtin_math_sin(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::sin(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_sinh(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::sinh(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_sqrt(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::sqrt(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_tan(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::tan(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
 int __builtin_math_tanh(Memory* memory, std::vector<std::size_t> arguments) {
+  if (arguments.size() != 2) LOGGING_ERROR("Invalid args");
+  auto memory_ptr = memory->GetMemory().data();
+  double result = std::tanh(GetDouble(memory_ptr + arguments[1]));
+  SetDouble(memory_ptr + arguments[0], result);
   return 0;
 }
 
